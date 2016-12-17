0ed98d3d3da03915c4c019812b2414c64d686e7c
Fredrik Sundberg <fredrik@sundbergmedia.com>
2016-12-17 19:04:04 +0100

    Added initial implementation of HTML parsing method

--- src/crawler.js
+++ src/crawler.js
@@ -1,5 +1,7 @@
 var Promise = require('bluebird');
 var request = require('request');
+var cheerio = require('cheerio');
+var _ = require('lodash');
 var userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36'; // default user-agent to use for requests
 var headers = {'User-Agent': userAgent};
 
@@ -15,6 +17,47 @@ function fetchUrl(url) {
   });
 }
 
+/*
+ * parse a HTML string using an object of jQuery-like selectors
+ */
+function parse(data, pattern, requiredSelector) {
+  return new Promise(function(resolve, reject) {
+    if(!requiredSelector) requiredSelector = 'body';
+    var results = {};
+    var $ = cheerio.load(data); // use cheerio for jQuery-like syntax
+    if($(requiredSelector).length < 1) resolve('error: did not found required selector');
+    _.each(pattern, function(item, key) {
+      var type = getType(item); // get item type for the current selector
+      var itemResults = [];
+      $(type.val).each(function() {
+        itemResults.push((type.type == "text")
+          ? $(this).text()
+          : $(this).attr(type.type)
+        );
+      });
+      results[key] = itemResults;
+    });
+    resolve(results);
+  });
+}
+
+function getType(val) {
+  var type = 'text'; // default type
+
+  if(val.indexOf(':') > 0) {
+    var p = val.split(':');
+    val   = p[0];
+    type  = p[1];
+  }
+
+  return {
+    type: type,
+    val: val
+  };
+}
+
+
 module.exports = {
   fetchUrl: fetchUrl,
+  parse: parse,
 }
