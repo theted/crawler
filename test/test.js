31eef54cfb4d1250326aa2a54e967795a66f0b79
Fredrik Sundberg <fredrik@sundbergmedia.com>
2016-12-18 02:29:15 +0100

    Updated tests

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 var Promise = require('bluebird');
+var _ = require('lodash');
 var crawler = require('../src/crawler');
 var config = require('./testConfig');
 var helpers = require('../src/helpers');
@@ -6,5 +7,27 @@ var log = helpers.log;
 
 log('Performing Crawler tests...');
 
+
 // fetch HTML of an URL
-crawler.fetchUrl(config.url).then(log);
+// crawler.fetchUrl(config.url).then(log);
+
+
+// fech an URL, then parse the HTML output
+crawler
+  
+  // first get the HTML from an URL
+  // .fetchUrl(config.url) // get raw HTML 
+  // .fetchUrlPhantom(config.url) // evaluate javascript
+  .fetchUrlPhantom(config.url, {wait: 3000}) // wait 3 s, evaluate javascript
+  // .then(log) // output HTML
+  
+  // parse HTML
+  .then(function(html) {
+    return crawler.parse(html, config.selectors);
+  })
+  .then(log) // output parsed HTML object   
+;
+
+
+// fetch an image and save it locally 
+crawler.fetchFile(config.fileRemote, config.fileLocal).then(log);
